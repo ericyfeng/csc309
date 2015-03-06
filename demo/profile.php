2c47464c7a6b9aacb65b9b535eb431eb4bda832a
Daniel Tjandra <daniel.tjandra@mail.utoronto.ca>
2015-03-06 17:54:48 -0500

    final bit of security for profile

--- demo/profile.php
+++ demo/profile.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
+<?php
+	session_start();
+?>
+
 <html>
 	<head>
 		<title>Simple Profile Management</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 		<script>
-			function addInterest (buttonid, commid, email)
+			function addInterest (buttonid, commid, sessid)
 			{
 				document.getElementById(buttonid).style.visibility="hidden";
 				updateInterest = new XMLHttpRequest();
@@ -13,7 +27,7 @@
 				}
 				updateInterest.open("POST", "updateInterest.php", true);
 				updateInterest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-				updateInterest.send("commid="+commid+"&email="+email);
+				updateInterest.send("commid="+commid+"&sessid="+sessid);
 			}
 			function verbose(v1, v2)
 			{
@@ -26,21 +40,66 @@
 		<?php
 			error_reporting(E_ALL);
 			ini_set('display_errors', 1);
-			$fname = $_GET["fname"];
-			$lname = $_GET["lname"];
-			$email = $_GET["user"];
+			date_default_timezone_set('America/Toronto');
 
 			$dbconn = pg_connect("dbname=cs309 user=Daniel");
+
+			//check if session id is real or faked
+			$sessid = $_GET["sessid"];
+			$validnum = "select count(*) from session where sessionid=$1";
+			pg_prepare($dbconn, "validnum", $validnum);
+			$result = pg_execute($dbconn, "validnum", array($sessid));
+			$row = pg_fetch_row($result);
+			if($row[0] == 0)
+			{
+				echo "Please login again"; //do not give hints about the failed login
+				exit();
+			}
+
+			//check if the valid session id # is expired or not
+			$expiry = "select expiration from session where sessionid=$1";
+			pg_prepare($dbconn, "expiry", $expiry);
+			$result = pg_execute($dbconn, "expiry", array($sessid));
+			$row = pg_fetch_row($result);
+			$dbdate = new DateTime($row[0]);
+			if($dbdate < new DateTime())
+			{
+				echo "Please login again";
+				exit();
+			}
+
+			$fname = $_SESSION["fname"];
+			$lname = $_SESSION["lname"];
+			$email = $_SESSION["email"];
+
+			//get current list of interested communities
 			$myinterest = "select description from personalinterests natural join community where email=$1";
 			pg_prepare($dbconn, "myinterest", $myinterest);
 			$result = pg_execute($dbconn, "myinterest", array($email));
 		?>
 
+		<!--Black nav bar-->
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<div class="navbar-brand"><?php echo $fname?> <?php echo $lname?>'s Scrap Funder</div>
+					</div>
+
+					<div class="navbar-nav navbar-right">
+							
+						<!--real log out button-->
+						<a href="logout.php?sessid=<?php echo $sessid?>" class="navbar-btn btn btn-primary"">
+							<span class="glyphicon glyphicon-off"></span> Log Out
+						</a>
+						</div>
+					</div>
+				</nav>
+
+		<div class="container">
 			<h1><?php echo"$fname $lname"?> 's Profile</h1>
 			<h3>My Interests</h3>
 				<div id="interests">
 					<ul>
-
 						<?php
 						while ($row = pg_fetch_row($result))
 						{
@@ -58,10 +117,11 @@
 				while($row = pg_fetch_row($result))
 				{
 					$id = $row[0];
-					echo "<button id=\"button$id\" onclick=\"addInterest('button$id', '$id', '$email')\"> \n $row[1] </button><br>\n";
+					echo "<button id=\"button$id\" onclick=\"addInterest('button$id', '$id', '$sessid')\"> \n $row[1] </button><br>\n";
 
 				}
 
 				?>
+		</div>
 	</body>
 </html>
--- demo/updateInterest.php
+++ demo/updateInterest.php
@@ -1,10 +1,43 @@
 <!DOCTYPE html>
+<?php
+	session_start();
+?>
+
 <html>
 	<body>
 		<?php
-			$commid = $_POST["commid"];
-			$email = $_POST["email"];
+			error_reporting(E_ALL);
+			ini_set('display_errors', 1);
+			date_default_timezone_set('America/Toronto');
 			$dbconn = pg_connect("dbname=cs309 user=Daniel");
+
+			//check if session id is real or faked
+			$sessid = $_POST["sessid"];
+			$validnum = "select count(*) from session where sessionid=$1";
+			pg_prepare($dbconn, "validnum", $validnum);
+			$result = pg_execute($dbconn, "validnum", array($sessid));
+			$row = pg_fetch_row($result);
+			if($row[0] == 0)
+			{
+				echo "Please login again"; //do not give hints about the failed login
+				exit();
+			}
+
+			//check if the valid session id # is expired or not
+			$expiry = "select expiration from session where sessionid=$1";
+			pg_prepare($dbconn, "expiry", $expiry);
+			$result = pg_execute($dbconn, "expiry", array($sessid));
+			$row = pg_fetch_row($result);
+			$dbdate = new DateTime($row[0]);
+			if($dbdate < new DateTime())
+			{
+				echo "Please login again";
+				exit();
+			}
+
+			$commid = $_POST["commid"];
+			$email = $_SESSION["email"];
+
 
 			$insert = "insert into personalinterests values ($1, $2)";
 			pg_prepare($dbconn, "insert", $insert);
