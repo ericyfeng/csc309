bf0744c3555b31cb054c2b27a23584867e3832ba
Daniel Tjandra <daniel.tjandra@mail.utoronto.ca>
2015-03-07 00:50:37 -0500

    fix add owner security hole

--- demo/addinit.php
+++ demo/addinit.php
@@ -41,8 +41,20 @@
 				exit();
 			}
 
-			//valid session id# that isn't expired, proceed to add the person as an initiator
+			//check for malicously direct fed data
 			$pid = $_POST["pid"];
+			$vemail = $_SESSION["email"];
+			$verifyowner = "select count(*) from initiator where email=$1 and projid=$2";
+			pg_prepare($dbconn, "verifyowner", $verifyowner);
+			$vresult = pg_execute($dbconn, "verifyowner", array($vemail, $pid));
+			$vrow = pg_fetch_row($vresult);
+			if($vrow[0] != 1)
+			{
+				echo "You aren't authorized to add owners";
+				exit();
+			}
+
+			//valid session id# that isn't expired, proceed to add the person as an initiator
 			$email = $_POST["email"];
 			$ins = "insert into initiator values ($1, $2)";
 			pg_prepare($dbconn, "ins", $ins);
