52ecc0376921c0aee4ecc821538c9647d3ff817b
ericyfeng <ericyfeng@hotmail.com>
2015-04-02 21:10:53 -0400

    trying to make index modular

--- web/index.php
+++ web/index.php
@@ -35,35 +35,10 @@
 		ini_set('display_errors', 1);
 		date_default_timezone_set('America/Toronto');
 		$dbconn = pg_connect("dbname=d8dt3b69jeev6n host=ec2-50-19-249-214.compute-1.amazonaws.com port=5432 user=fhntmyljqrdquf password=vgJO4ZQS8Mi7OceXpIzk_dYL0- sslmode=require");
+		include("template/navbar.php");
 	?>
 
-	<! ========== NAV BAR ==================================================================================================== 
-	=============================================================================================================================>
 
-    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
-        <div class="container topnav">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <a class="navbar-brand topnav" href="index.php">HOME</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="explore.php">Explore</a>
-                    </li>
-                    <li>
-                        <a href="#signup" data-toggle="modal">Sign up</a>
-                    </li>
-                    <li>
-                        <a href="#login" data-toggle="modal">Login</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
 
 	<! ========== HEADERWRAP ==================================================================================================== 
@@ -269,41 +244,7 @@
 	<! ========== FOOTER ======================================================================================================== 
 	=============================================================================================================================>    
 	
-	<div id="f">
-		<div class="container">
-			<div class="row">
-				<!-- ADDRESS -->
-				<div class="col-lg-3">
-					<h4>Contact Us</h4>
-					<p>
-						<i class="fa fa-mobile"></i> +1-123-3902<br/>
-						<i class="fa fa-envelope-o"></i> support@brandname.com
-					</p>
-				</div>  <!--/col-lg-3 -->
-				
-
-				<!-- LATEST POSTS -->
-				<div class="col-lg-3">
-					<h4>Latest Projects</h4>
-					<p>
-						<i class="fa fa-angle-right"></i> The Ninja Film<br/>
-						<i class="fa fa-angle-right"></i> Water proof bananas<br/>
-						<i class="fa fa-angle-right"></i> Strike Money<br/>
-						<i class="fa fa-angle-right"></i> Smart Headphones<br/>
-						<i class="fa fa-angle-right"></i> 50 shades of blue novel<br/>
-					</p>
-				</div><!-- /col-lg-3 -->
-				
-				<!-- NEW PROJECT -->
-				<div class="col-lg-3">
-					<h4>New Project</h4>
-					<a href="#"><img class="img-responsive" src="assets/img/portfolio/port03.jpg" alt="" /></a>
-				</div><!-- /col-lg-3 -->
-				
-				
-			</div> <!--/row -->
-		</div><!-- /container -->
-	</div><!-- /f -->
+	<?php include("template/footer.php"); ?>
 
 	<! ========== MODALS ======================================================================================================== 
 	=============================================================================================================================>    
---
+++ web/template/navbar.php
@@ -0,0 +1,27 @@
+	<! ========== NAV BAR ==================================================================================================== 
+	=============================================================================================================================>
+
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand topnav" href="index.php">HOME</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="explore.php">Explore</a>
+                    </li>
+                    <li>
+                        <a href="#signup" data-toggle="modal">Sign up</a>
+                    </li>
+                    <li>
+                        <a href="#login" data-toggle="modal">Login</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
