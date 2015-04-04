23f94afd0f0f6f2ef1385d39507dc78b2a50c170
ericyfeng <ericyfeng@hotmail.com>
2015-04-04 18:40:08 -0400

    explore templating

--- web/explore.php
+++ web/explore.php
@@ -1,31 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="assets/ico/favicon.png">
 
+  	<?php include("template/head.php"); ?>
     <title>Explore</title>
 
-    <link href="assets/css/hover_pack.css" rel="stylesheet">
-
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="assets/css/main.css" rel="stylesheet">
-    <link href="assets/css/colors/color-74c9be.css" rel="stylesheet">    
-    <link href="assets/css/animations.css" rel="stylesheet">
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
     <link href="assets/css/explore.css" rel="stylesheet">       
-    
-    <!-- Main Jquery & Hover Effects. Should load first -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="assets/js/hover_pack.js"></script>
-    
 
   </head>
 
@@ -36,34 +16,20 @@
 		ini_set('display_errors', 1);
 		date_default_timezone_set('America/Toronto');
 		$dbconn = pg_connect("dbname=d8dt3b69jeev6n host=ec2-50-19-249-214.compute-1.amazonaws.com port=5432 user=fhntmyljqrdquf password=vgJO4ZQS8Mi7OceXpIzk_dYL0- sslmode=require");
+
+		//enable php debugging
+		if($_SESSION["loggedin"] == 1)
+		{
+			include("template/loginnav.php");
+			//include("template/navbar.php");
+		}
+		else
+		{
+			include("template/navbar.php");
+		}
 	?>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
-        <div class="container topnav">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <a class="navbar-brand topnav" href="index.php">Brand Name</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="#">Explore</a>
-                    </li>
-                    <li>
-                        <a href="#signup" data-toggle="modal">Sign up</a>
-                    </li>
-                    <li>
-                        <a href="#login" data-toggle="modal">Login</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
+]	
 	<!--Surround the body in its own container-->
 	<div class="container-fliud">
 		<!--Everything in the body will be in a single row-->
@@ -74,6 +40,7 @@
 					<div class="container-fliud pull-right">
 						<ul class="nav">
 							<li>Categories</li>
+								<li><a onclick="filtertags('commgeneric')">ALL</a></li>
 								<?php
 									//for now just pull any project in the backend to display
 									$categories = "select description from community;";
@@ -184,118 +151,11 @@
 	<! ========== FOOTER ======================================================================================================== 
 	=============================================================================================================================>    
 	
-	<div id="f">
-		<div class="container">
-			<div class="row">
-				<!-- ADDRESS -->
-				<div class="col-lg-3">
-					<h4>Contact Us</h4>
-					<p>
-						<i class="fa fa-mobile"></i> +1-123-3902<br/>
-						<i class="fa fa-envelope-o"></i> support@brandname.com
-					</p>
-				</div>  <!--/col-lg-3 -->
-				
-
-				<!-- LATEST POSTS -->
-				<div class="col-lg-3">
-					<h4>Latest Projects</h4>
-					<p>
-						<i class="fa fa-angle-right"></i> The Ninja Film<br/>
-						<i class="fa fa-angle-right"></i> Water proof bananas<br/>
-						<i class="fa fa-angle-right"></i> Strike Money<br/>
-						<i class="fa fa-angle-right"></i> Smart Headphones<br/>
-						<i class="fa fa-angle-right"></i> 50 shades of blue novel<br/>
-					</p>
-				</div><!-- /col-lg-3 -->
-				
-				<!-- NEW PROJECT -->
-				<div class="col-lg-3">
-					<h4>New Project</h4>
-					<a href="#"><img class="img-responsive" src="assets/img/portfolio/port03.jpg" alt="" /></a>
-				</div><!-- /col-lg-3 -->
-				
-				
-			</div> <!--/row -->
-		</div><!-- /container -->
-	</div><!-- /f -->
+	<?php include("template/footer.php"); ?>
 
 	<! ========== MODAL ======================================================================================================== 
 	=============================================================================================================================>    
-    <div class="modal fade" id="signup" tabindex="-1" role= "dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h4>Sign Up</h4>
-				</div>
-				<div class="modal-body">
-
-						<div class="form-group">
-							<label for="fname">First Name:</label>
-							<input type="text" class="form-control" id="fname" name="fname" placeholder="Please Enter Your First Name">
-						</div>
-						<div class="form-group">
-							<label for="text">Last Name:</label>
-							<input type="text" class="form-control" id="lname" name="lname" placeholder="Please Enter Your Last Name">
-						</div>				
-						<div class="form-group">
-							<label for="email">Email:</label>
-							<input type="email" class="form-control" id="email" name="email" placeholder="Please Enter Your Email">
-						</div>
-						<div class="form-group">
-							<label for="pwd">Password:</label>
-							<input type="password" class="form-control" id="passwd" name="passwd"  placeholder="Please Enter Your Password">
-						</div>
-						<div class="form-group">
-							<label for="repwd">Confirm Password:</label>
-							<input type="password" class="form-control" id="confirm" name="confirm" placeholder="Please Re-Enter Your Password">
-						</div>
-						<div class="modal-footer">
-				      		<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-				      		<button type="button" class="btn btn-primary" onclick="regjax()">Sign Up!</button>
-	  					</div>						
-
-					<p id="status"></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="modal fade" id="login" tabindex="-1" role= "dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h4>Login</h4>
-				</div>
-				<div class="modal-body">
-					<form action="login.php" method="POST">
-						<div class="form-group">
-							<label for="email">Email:</label>
-							<input type="email" class="form-control" id="email" name="email" placeholder="Please Enter Your Email">
-						</div>
-						<div class="form-group">
-							<label for="pwd">Password:</label>
-							<input type="password" class="form-control" id="pwd" name="passwd" placeholder="Please Enter Your Password">
-						</div>
-						<div class="checkbox">
-							<label><input type="checkbox">Remember Me</label>
-						</div>
-						<div class="extralogin">
-							<a href="#">Forgot your password?</a>
-							<p>Don't have an account?
-								<a href="#signup" data-toggle="modal" data-dismiss="modal">Register now!</a>
-							</p>	
-						</div>
-						<div class="modal-footer">
-					      	<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-					      	<input type="submit" class="btn btn-primary" value="Log In">
-  						</div>
-					</form>
-				</div>
-
-			</div>
-		</div>
-	</div>
-
+ 	<?php include("template/usermodals.php"); ?>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
